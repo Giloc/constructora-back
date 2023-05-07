720c1660fc8314678f612759727ee0d0395891ae
Giloc Delgado <giloc.delgado@udea.edu.co>
2023-05-06 19:29:33 -0500

    troleando

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://giloc:Bateria98.@cluster0.i4tx3om.mongodb.net/constructora")
+mongoose.connect("mongodb+srv://giloc:contra@cluster0.i4tx3om.mongodb.net/constructora")
     .then(db => console.log("Db is connected"))
     .catch(err => console.log(err))
